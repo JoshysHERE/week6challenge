24f002e0fb237495426e3c69abbdddfa84f89be6
Joshua <Joshua.soto@live.com>
2023-09-27 21:34:21 -0500

    added code for open weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
 let weather = {
-    "apiKey:095780be0d2a0e556ee508b10271a24d "
+    "apiKey":  "095780be0d2a0e556ee508b10271a24d",
+    fetchWeather: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=095780be0d2a0e556ee508b10271a24d&units=imperial"
+            + city
+            + "&units=metric&appid="
+            + this.appKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+},
+displayWeather: function(data) {
+    var { name } = data;
+    var { icon, description } = data.weather[0];
+    var { temp, humidity } = data.main
+    var { speed } = data.wind
+    document.querySelector('city').innerText = 'Weather in' + name;
+    document.querySelector('.icon').src = 
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector('description').innerText = description;
+    document.querySelector('temp').innerText = temp + '°F';
+    document.querySelector('humidity').innerText = 'humidity: ' + humidity + '%';
+    document.querySelector('wind').innerText = 'wind speed: ' + speed + 'km/h';
+ }.
+search: function () {
+    this.fetchWeather(document.querySelector('.search').value);
+  }
+};
+
+document.querySelector('search').addEventListener('click', function () {
+    weather.search();
+});
+
+document.querySelector('search').addEventListener('keyup', function (event) { 
+if (event.key == 'enter') {
+weather.search
 }
+});
