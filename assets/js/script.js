5a5a4195daa792eef33e89395c398242b1ec3567
Joshua <Joshua.soto@live.com>
2023-09-28 17:29:11 -0500

    added icon for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,40 @@
 let weather = {
     "apiKey":  "095780be0d2a0e556ee508b10271a24d",
-    fetchWeather: function() {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=095780be0d2a0e556ee508b10271a24d&units=imperial"
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
             + city
-            + "&units=metric&appid="
+            + "&units=imperial&appid="
             + this.appKey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
 },
 displayWeather: function(data) {
     var { name } = data;
     var { icon, description } = data.weather[0];
-    var { temp, humidity } = data.main
-    var { speed } = data.wind
-    document.querySelector('city').innerText = 'Weather in' + name;
+    var { temp, humidity } = data.main;
+    var { speed } = data.wind;
+
+    document.querySelector('.city').innerText = 'Weather in' + name;
     document.querySelector('.icon').src = 
-    "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector('description').innerText = description;
-    document.querySelector('temp').innerText = temp + '°F';
-    document.querySelector('humidity').innerText = 'humidity: ' + humidity + '%';
-    document.querySelector('wind').innerText = 'wind speed: ' + speed + 'km/h';
- }.
+    "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=radar&lat=51.5085&lon=-0.1257&zoom=6" + icon + ".png";
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = temp + '°F';
+    document.querySelector('.humidity').innerText = 'humidity: ' + humidity + '%';
+    document.querySelector('.wind').innerText = 'wind speed: ' + speed + 'km/h';
+}.
 search: function () {
-    this.fetchWeather(document.querySelector('.search').value);
-  }
+    this.fetchWeather(document.querySelector('.search-bar').value);
+  },
 };
 
-document.querySelector('search').addEventListener('click', function () {
+document.querySelector('.search button').addEventListener('click', function () {
     weather.search();
 });
 
-document.querySelector('search').addEventListener('keyup', function (event) { 
-if (event.key == 'enter') {
-weather.search
+document.querySelector('.search').addEventListener('keyup', function (event) { 
+if (event.key == 'Enter') {
+weather.search();
 }
 });
